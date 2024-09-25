edba52c9eb5dccd89e53c976f60ee3df5eeba7f5
smbrmoyo <brianmoyou@gmail.com>
2024-09-25 14:11:41 +0200

    Fixed Refresh Bug on the ScrollViews

---
+++ GitHub Insights/Views/UserRepos/UserRepositoriesView.swift
@@ -0,0 +1,18 @@
+//
+//  UserRepositoriesView.swift
+//  GitHub Insights
+//
+//  Created by Brian Moyou on 23.09.24.
+//
+
+import SwiftUI
+
+struct UserRepositoriesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserRepositoriesView()
+}
---
+++ GitHub Insights/Views/UserRepos/UserRepositoriesViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserRepositoriesViewModel.swift
+//  GitHub Insights
+//
+//  Created by Brian Moyou on 23.09.24.
+//
+
+import Foundation
