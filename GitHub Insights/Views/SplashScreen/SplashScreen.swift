741f29c41b9b83687c728039f042f998e7e5a071
smbrmoyo <brianmoyou@gmail.com>
2024-09-26 14:18:47 +0200

    Added SplashScreen

---
+++ GitHub Insights/Views/SplashScreen/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  GitHub Insights
+//
+//  Created by Brian Moyou on 26.09.24.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
